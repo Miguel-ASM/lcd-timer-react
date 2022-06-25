11b259ad5d2b20907e926c9837080e768393f517
Miguel-ASM <miguel.a.s.martinez@gmail.com>
2022-06-25 13:21:56 +0200

    First working version of timer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import "./App.scss";
+import { Button } from "antd";
 import Display from "./components/Display/Display";
 import { secondsToDisplayString } from "./helpers/helpers";
+import { useEffect, useState } from "react";
 
 function App() {
-  console.log(secondsToDisplayString(159));
+  const [seconds, setSeconds] = useState(0);
+  const [timerRunning, setTimerRunning] = useState(false);
+  const [runningTimeOut, setRunningTimeOut] = useState(null);
+
+  useEffect(() => {
+    if (timerRunning) {
+      const newTimeOut = setTimeout(() => {
+        setSeconds((seconds) => {
+          return seconds + 1;
+        });
+      }, 1000);
+      setRunningTimeOut(newTimeOut);
+    }
+  }, [seconds, timerRunning]);
+
+  const toggleIsRunnning = () => {
+    if (timerRunning) clearTimeout(runningTimeOut);
+    setTimerRunning((x) => !x);
+  };
+
+  const resetTimer = () => {
+    setSeconds(0);
+    setTimerRunning(false);
+  };
+
   return (
     <div className="App">
-      <Display digitsString={"10:00"} />
+      <Display digitsString={secondsToDisplayString(seconds)} />
+      <Button type="primary" onClick={toggleIsRunnning}>
+        {timerRunning ? "PAUSE" : seconds === 0 ? "START" : "RESUME"}
+      </Button>
+      {!timerRunning && (
+        <Button type="primary" onClick={resetTimer}>
+          RESET
+        </Button>
+      )}
     </div>
   );
 }
