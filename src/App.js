e41521dbdadb0e90f381c4f74b8ddfd62cf2940d
Miguel-ASM <miguel.a.s.martinez@gmail.com>
2022-06-25 10:56:23 +0200

    markup and css for lcd display digits

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.scss";
+import Digit from "./components/Digit/Digit";
+
+function App() {
+  return (
+    <div className="App">
+      <Digit />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Digit/Digit.js
@@ -0,0 +1,15 @@
+import "./Digit.scss";
+
+export default function Digit() {
+  return (
+    <div class="display-digit display-digit-9">
+      <div class="display-digit-bar display-digit-bar-top"></div>
+      <div class="display-digit-bar display-digit-bar-top-left"></div>
+      <div class="display-digit-bar display-digit-bar-top-right"></div>
+      <div class="display-digit-bar display-digit-bar-middle"></div>
+      <div class="display-digit-bar display-digit-bar-bottom-left"></div>
+      <div class="display-digit-bar display-digit-bar-bottom-right"></div>
+      <div class="display-digit-bar display-digit-bar-bottom"></div>
+    </div>
+  );
+}
