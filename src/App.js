b3ea90bff31780113bf42c11cb9b4ca15a18f25a
Miguel-ASM <miguel.a.s.martinez@gmail.com>
2022-06-26 18:40:49 +0200

    Included cent seconds in timer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import "./App.scss";
 import { Button } from "antd";
 import Display from "./components/Display/Display";
-import { secondsToDisplayString } from "./helpers/helpers";
+import { centiSecondsToDisplayString } from "./helpers/helpers";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [seconds, setSeconds] = useState(0);
+  const [centiSeconds, setCentiSeconds] = useState(0);
   const [timerRunning, setTimerRunning] = useState(false);
   const [runningTimeOut, setRunningTimeOut] = useState(null);
 
   useEffect(() => {
     if (timerRunning) {
       const newTimeOut = setTimeout(() => {
-        setSeconds((seconds) => {
-          return seconds + 1;
+        setCentiSeconds((centiSeconds) => {
+          return centiSeconds + 1;
         });
-      }, 1000);
+      }, 10);
       setRunningTimeOut(newTimeOut);
     }
-  }, [seconds, timerRunning]);
+  }, [centiSeconds, timerRunning]);
 
   const toggleIsRunnning = () => {
     if (timerRunning) clearTimeout(runningTimeOut);
@@ -26,14 +26,14 @@ function App() {
   };
 
   const resetTimer = () => {
-    setSeconds(0);
+    setCentiSeconds(0);
     setTimerRunning(false);
   };
 
   return (
     <div className="App">
       <div className="timer">
-        <Display digitsString={secondsToDisplayString(seconds)} />
+        <Display digitsString={centiSecondsToDisplayString(centiSeconds)} />
         <div className="timer_buttons">
           {!timerRunning && (
             <Button
@@ -49,7 +49,7 @@ function App() {
             type="primary"
             onClick={toggleIsRunnning}
           >
-            {timerRunning ? "PAUSE" : seconds === 0 ? "START" : "RESUME"}
+            {timerRunning ? "PAUSE" : centiSeconds === 0 ? "START" : "RESUME"}
           </Button>
         </div>
       </div>
--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -1,21 +1,20 @@
 import Digit from "../Digit/Digit";
 import "./Display.scss";
 export default function Display({ digitsString }) {
-  //digitsString = XX:YY
-  const minutesTen = digitsString.split(":")[0][0];
-  const minutes = digitsString.split(":")[0][1];
-  const secondsTen = digitsString.split(":")[1][0];
-  const seconds = digitsString.split(":")[1][1];
+  //digitsString = mm:SS.ss
+  const [minutes, seconds, centiSeconds] = digitsString.split(/[:.]/);
   return (
     <div className="timer-display">
-      <Digit digit={minutesTen} />
-      <Digit digit={minutes} />
+      <Digit digit={minutes[0]} />
+      <Digit digit={minutes[1]} />
       <div className="points-container">
         <div className="point"></div>
         <div className="point"></div>
       </div>
-      <Digit digit={secondsTen} />
-      <Digit digit={seconds} />
+      <Digit digit={seconds[0]} />
+      <Digit digit={seconds[1]} />
+      <Digit digit={centiSeconds[0]} decimal={true} />
+      <Digit digit={centiSeconds[1]} decimal={true} />
     </div>
   );
 }
